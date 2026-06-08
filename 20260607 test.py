179d19c1952123b4b0a4f0c9d322364aca8e6923
NERVbowen <48186358+NERVbowen@users.noreply.github.com>
2026-06-07 21:27:42 -0400

    Add files via upload

---
+++ 20260607 test.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from molmass import Formula
+
+st.title("MS Calculator")
+
+formula = st.text_input("Molecular Formula", "C8H10N4O2")
+
+if formula:
+    try:
+        mw = Formula(formula).isotope.mass
+
+        st.write(f"Monoisotopic Mass = {mw:.5f}")
+        st.subheader("Positive Mode")
+
+        st.write(f"[M+H]+ = {mw + 1.00783:.5f}")
+        st.write(f"[M+NH4]+ = {mw + 18.03437:.5f}")
+        st.write(f"[M+Na]+ = {mw + 22.98922:.5f}")
+        st.write(f"[M+K]+ = {mw + 38.96316:.5f}")
+
+        st.subheader("Negative Mode")
+
+        st.write(f"[M-H]- = {mw - 1.00783:.5f}")
+        st.write(f"[M+Cl]- = {mw + 34.96885:.5f}")
+        st.write(f"[M+FA-H]- = {mw + 44.99820:.5f}")
+        st.write(f"[M+Ac-H]- = {mw + 59.01385:.5f}")
+
+    except Exception as e:
+        st.error("Invalid formula")
